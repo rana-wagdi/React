ef559c3eb724e2713e89bd20a0c002b74266c027
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-04 05:49:19 -0800

    jsx

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      //we can add just one component
+      <div className="App"> 
       <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this work now?'));
