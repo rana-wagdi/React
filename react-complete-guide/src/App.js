571198dda724a2e74b346119295238ee052b1ada
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-05 09:41:51 -0800

    switch button

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,8 +7,17 @@ class App extends Component {
     persons: [
       { name: "Rana", age: 23 },
       { name: "Ahmed", age: 28 },
-      { name: "Wagdi", age: 60 }
+      { name: "Wagdi", age: 60 },
     ]
+  };
+  switchHandler = () => {
+    this.setState({
+      persons: [
+        { name: "roshy", age: 23 },
+        { name: "medo", age: 28 },
+        { name: "Wago", age: 60 },
+      ],
+    });
   };
   render() {
     return (
@@ -16,12 +25,21 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch button</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+        <button onClick={this.switchHandler}>Switch button</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My Hobbies: Racing
         </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this work now?'));
