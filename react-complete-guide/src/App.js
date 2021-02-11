e76bea060adf8890bf832f89a81fdd500981feb9
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-11 08:02:15 -0800

    handling Dynamic "the JavaScript Way"

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -40,33 +40,38 @@ togglePersonHandler =()=>{
 }
 
   render(){
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchHandler.bind(this, "rora")}
+            changed={this.nameEventHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       //we can add just one component
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button onClick={this.togglePersonHandler}>Switch button</button>
-        {
-          this.state.showPersons === true ?
-          <div>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchHandler.bind(this,'rora')}
-          changed={this.nameEventHandler}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-        </div> :null
-  }
+      {persons}
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this work now?'));
