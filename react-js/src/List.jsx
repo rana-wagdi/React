d0987d8e13bc9a91bdc0ec8471a9fc175b5b2985
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-14 12:36:34 -0700

    add heading

---
+++ react-js/src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List(){
+    return (
+      <ul>
+        <li>chicken</li>
+        <li>mahshy</li>
+        <li>besla</li>
+      </ul>
+    );
+}
+export default List;
