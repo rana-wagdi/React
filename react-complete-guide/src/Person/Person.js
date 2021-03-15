78da2f1fb80e30b21f17ba78dff436f5605830d7
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-15 11:35:49 -0700

    add styled

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,14 +1,30 @@
 import React from'react';
-import './person.css';
+// import './person.css';
+import styled from 'styled-components';
+
+const StyleDiv = styled.div `
+  width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+
+  @media (min-width: 500px) {
+    width: 450px;
+}
+`
+
 const person =(Props)=>{
     return (
-      <div className='person'>
-        <p onClick={Props.click} >
+      <StyleDiv>
+        <p onClick={Props.click}>
           I'm a {Props.name} and I am {Props.age} years old !
         </p>
         <p>{Props.children}</p>
-       <input type="text" onChange ={Props.changed}></input>
-      </div>
+        <input type="text" onChange={Props.changed}></input>
+      </StyleDiv>
     );
 }
 
