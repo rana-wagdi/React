821fc5232ab741df367c5e14ab117eeb93966ec1
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-15 11:55:46 -0700

    styled components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 import Person from './Person/Person';
 
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor:pointer;
+
+  &:hover{
+    background-color: lightgreen;
+    color: black;
+  }
+
+`;
 
 class App extends Component {
   state = {
@@ -76,7 +91,7 @@ if (this.state.persons.length <= 1) {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button onClick={this.togglePersonHandler}>Switch button</button>
+        <StyledButton onClick={this.togglePersonHandler}>Toggle Persons</StyledButton>
         {persons}
       </div>
     );
