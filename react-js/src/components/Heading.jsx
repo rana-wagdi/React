f7089f8b50c6877ab40a641d54a94090a28650bc
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-21 02:19:34 -0700

    intial commit

--- react-js/src/components/Heading.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Heading(){
-    return <h1>My favourite foods</h1>
-}
-export default Heading;
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+ReactDOM.render(<h1>Hello word</h1>, document.getElementById('root'));
+
