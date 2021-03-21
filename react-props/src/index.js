d33ed26daabf1b686ff46e060d310e13ad55b33d
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-21 14:13:57 -0700

    react props

--- react-props/src/index.js
+++ react-props/src/index.js
@@ -1,23 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Card from './component/Card'
 
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img
-        src={props.img}
-        alt="avatar_img"
-      />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
 
 ReactDOM.render(
   <div>
-    <h1>My Contacts</h1>
+    <h1 className="heading">My Contacts</h1>
     <Card
       name="Beyonce"
       img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
