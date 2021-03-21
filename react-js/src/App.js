d5151c4ad8db013daf327049429ddffd3d704001
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-21 01:55:02 -0700

    import ,,export

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Index from "./components/Index";
-
+import  * as Calculator from "./Calculator";
 class App extends Component {
   render() {
     return (
-      
-        <Index />
-      
+      <div>
+        <ul>
+          <li>{Calculator.add(1, 2)}</li>
+          <li>{Calculator.multiply(2, 3)}</li>
+          <li>{Calculator.subtract(7, 2)}</li>
+          <li>{Calculator.divide(5, 2)}</li>
+        </ul>
+      </div>
     );
   }
 }
