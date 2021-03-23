1c835abbe08a4760f1e78d17776d6b8d0f2bd066
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-22 23:16:22 -0700

    mapping

--- react-props/src/component/App.jsx
+++ react-props/src/component/App.jsx
@@ -3,7 +3,9 @@ import Card from "./Card";
 import contacts from "../contacts"
 
 function createCard(contact) {
-  return <Card name ={contact.name}
+
+  return <Card key = {contact.id}
+              name ={contact.name}
               img = {contact.imgURL}
               tel = {contact.phone}
               email = {contact.email}  />
@@ -16,24 +18,7 @@ return (
     <h1 className="heading">My Contacts</h1>
 
     {contacts.map(createCard)}
-    {/* <Card
-      name={contacts[0].name}
-      img={contacts[0].imgURL}
-      tel={contacts[0].phone}
-      email={contacts[0].email}
-    />
-    <Card
-      name={contacts[1].name}
-      img={contacts[1].imgURL}
-      tel={contacts[1].phone}
-      email={contacts[1].email}
-    />
-    <Card
-      name={contacts[2].name}
-      img={contacts[2].imgURL}
-      tel={contacts[2].phone}
-      email={contacts[2].email}
-    /> */}
+
   </div>
 );
 }
