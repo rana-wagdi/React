0c67924cf7d11382c66463f1d00b7e3a8bd2b1c0
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-23 23:06:14 -0700

    mapping data

--- mapping-components/src/components/App.jsx
+++ mapping-components/src/components/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Entry from "./Entry"
+import emojipedia from "../emojipedia";
 
+
+function crateEmoji(emoj){
+  return <Entry emoji={emoj.emoji} name={emoj.name} mean={emoj.meaning} />;
+}
 function App() {
   return (
     <div>
@@ -7,48 +13,9 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
-      </dl>
-    </div>
-  );
+      {emojipedia.map(crateEmoji)}
+</div>
+        );
 }
 
 export default App;
--- mapping-components/src/emojipedia.js
+++ mapping-components/src/emojipedia.js
@@ -4,20 +4,28 @@ const emojipedia = [
     emoji: "💪",
     name: "Tense Biceps",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
   },
   {
     id: 2,
     emoji: "🙏",
     name: "Person With Folded Hands",
     meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
   },
   {
     id: 3,
     emoji: "🤣",
     name: "Rolling On The Floor, Laughing",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  }
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😼 ",
+    name: "Cat Face With Wry Smile",
+    meaning:
+      "Cat is laughing daringly with boldly raised corners of the mouth. It mocks you, is sarcastic or having fun at your expense. Might fancy a flirt. However, you had better be cautious.",
+  },
 ];
+export default emojipedia;
