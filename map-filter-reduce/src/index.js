9c9aee975025796138a409daf5b911244b3ba1b5
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-25 09:21:07 -0700

    findIndex

--- map-filter-reduce/src/index.js
+++ map-filter-reduce/src/index.js
@@ -46,8 +46,12 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Find - find the first item that matches from an array.
 
-const newNumber = numbers.find(function(num){
-    return num > 10
-})
-console.log(newNumber)
+// const newNumber = numbers.find(function(num){
+//     return num > 10
+// })
+// console.log(newNumber)
 //FindIndex - find the index of the first item that matches.
+const newNumber = numbers.findIndex(function (num) {
+  return num > 10;
+});
+console.log(newNumber);
