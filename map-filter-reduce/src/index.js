efb5f20ac5889b5b9494bd213c4da38ca38cbb50
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-25 08:55:10 -0700

    add filter

--- map-filter-reduce/src/index.js
+++ map-filter-reduce/src/index.js
@@ -2,12 +2,30 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
-const newNumbers= numbers.map(function(x){
-    return x * 2;
+// const newNumbers= numbers.map(function(x){
+//     return x * 2;
     
-})
+// })
+// console.log(newNumbers);
 
 //Filter - Create a new array by keeping the items that return true.
+
+// const newNumber = numbers.filter(function(num){
+//     return num > 10;
+// })
+// console.log(newNumber);
+
+
+
+// hn3mleha l forEach
+
+var newNumbers = [];
+numbers.forEach(function(num){
+    if (num < 10){
+        newNumbers.push(num)
+    }
+    console.log(newNumbers)
+})
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
