bbda8f9c885c3606e1833be4ad59f45fc2191199
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-26 12:11:37 -0700

    mapping notes

--- keeper-project/src/index.js
+++ keeper-project/src/index.js
@@ -3,11 +3,16 @@ import ReactDom from "react-dom";
 import Header from './component/Header';
 import Note from './component/Note';
 import Footer from './component/Footer';
+import notes from './notes';
+
+
 
 ReactDom.render(
   <div>
     <Header />
-    <Note />
+    {notes.map((noteItem) => (
+      <Note title={noteItem.title} content={noteItem.content} />
+    ))}
     <Footer />
   </div>,
 
