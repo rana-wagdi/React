5ae9bd8507c4f2f39d376228c53b4fa45a81ed15
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-27 06:17:45 -0700

    ternary operator

--- react-conditional/src/components/App.jsx
+++ react-conditional/src/components/App.jsx
@@ -3,14 +3,18 @@ import Login from "./Login"
 
 var isLoggedIn = false;
 
+const currentTime = new Date().getHours();
 
+console.log(currentTime);
 
 
 function App() {
   return (
     <div className="container">{
-      isLoggedIn === true ? <h1>Hello</h1>
- : <Login />
+//       isLoggedIn === true ? <h1>Hello</h1>
+//  : <Login />
+
+      currentTime <=12 ? <h1>GOOd morning</h1> : <h1>Good evening</h1>
    
   
      } </div>
