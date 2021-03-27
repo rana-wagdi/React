d83e573794a6e45bba9a0f888adcf10669598bc5
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-27 04:21:17 -0700

    intial commit

---
+++ react-conditional/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+var isLoggedIn = false;
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
