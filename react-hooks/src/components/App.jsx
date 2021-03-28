e63402da39e2914139f5fd72f52ce4ffebf215b3
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-28 02:46:46 -0700

    update

--- react-hooks/src/components/App.jsx
+++ react-hooks/src/components/App.jsx
@@ -1,20 +1,29 @@
 import React, {useState} from "react";
 
 function App(){
-  const [count, setState] = useState(0);
+  // const [count, setState] = useState(0);
 
-  function increase(){
-    setState(count+1);
-  }
-  function decrease(){
-    setState(count-1);
-  }
+  // function increase(){
+  //   setState(count+1);
+  // }
+  // function decrease(){
+  //   setState(count-1);
+  // }
+  setInterval(updateTime, 1000);
+  const now = new Date().toLocaleTimeString();
+
+
+  const [time, setTime] = useState(now) ;
+
+function updateTime(){
+  const newTime = new Date().toLocaleTimeString();
+  setTime(newTime)
+}
+    
 return (
-  <div>
-    <h1>{count}</h1>
-
-    <button onClick={decrease}>-</button>
-    <button onClick={increase}>+</button>
+  <div className="container">
+    <h1>{time}</h1>
+    <button onClick={updateTime}>Get Time</button>
   </div>
 );
 }
