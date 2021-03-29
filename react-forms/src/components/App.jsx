9e0d2e978615359f95e6cc73f58ed9297042f2f1
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-03-29 11:14:10 -0700

    intial commit

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -2,17 +2,27 @@ import React, {useState} from "react";
 
 function App() {
 
-  const [name, setName] = useState("")
-
+  const [name, setName] = useState("");
+  const [handleText, setHeading] = useState("")
 function handleChange(event){
-  setName(event.target.value)
+  setName(event.target.value);
 }
+function handleClick(){
+  setHeading(name)
+}
+
 
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button onChange = {handleChange}>Submit</button>
+      <h1>Hello {handleText}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button onClick={handleClick} value={name}>
+        Submit
+      </button>
     </div>
   );
 }
