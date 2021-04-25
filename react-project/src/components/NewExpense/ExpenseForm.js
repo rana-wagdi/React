2184934f6998465f6ae81612c2f7e9c2a7f66c62
rana2020701 <ranawagdi4@gmail.com>
2021-04-25 04:06:55 +0200

    add components

---
+++ react-project/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+    return (
+        <form>
+            <div>
+                <div>
+                    <label>Title</label>
+                    <input type = 'text' />
+                </div>
+            </div>
+        </form>
+    )
+}
+
+
+export default ExpenseForm;
