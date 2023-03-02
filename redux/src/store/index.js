aa873255a15193a5626ab6ec11448932f2250e40
rana-wagdi <ranawagdi4@gmail.com>
2023-03-02 11:24:16 +0200

    multiple state

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,18 +1,34 @@
 import { createStore } from "redux";
 
-const createReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+const createReducer = (state = initialState, action) => {
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
   }
   if (action.type === "increase") {
-    return { counter: state.counter + action.amount };
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
   }
   if (action.type === "decrement") {
     if (state.counter === 0) {
       alert("raana");
     } else {
-      return { counter: state.counter - 1 };
+      return {
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };    
     }
+  }
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
   }
   return state;
 };
