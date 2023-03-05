6908fbb6cf4ab655733e98118c6dc0b32a7737b2
rana-wagdi <ranawagdi4@gmail.com>
2023-03-05 14:23:55 +0200

    add multiple slices

--- redux/src/components/Auth.js
+++ redux/src/components/Auth.js
@@ -1,6 +1,13 @@
 import classes from './Auth.module.css';
-
+import { authAction } from '../store';
+import { useDispatch, useSelector } from 'react-redux';
 const Auth = () => {
+  const dispatch = useDispatch()
+  // const auth = useSelector(state.isAuthenticated)
+
+  const login = () =>  {
+    dispatch(authAction.login())
+  }
   return (
     <main className={classes.auth}>
       <section>
@@ -13,7 +20,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          <button onClick={login}>Login</button>
         </form>
       </section>
     </main>
